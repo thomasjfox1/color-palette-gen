566b483320605503d07560f5a4626350f399761b
Thomas J. Fox <thomasjfox1@gmail.com>
2015-02-05 15:39:06 -0600

    added interactivity with the palette, hover effects, changed font

--- www/public_src/scripts/controllers/homepage.js
+++ www/public_src/scripts/controllers/homepage.js
@@ -11,7 +11,9 @@ angular.module('color-palatte-gen')
             $scope.colorInfo = results;
             sizing(results.number);
             // $scope.hexValues = ['#002635', '#013440', '#AB1A25', '#D97925', '#123456', '#123123', '#456456', '#789789', '#987987', '#654654'];
-            $scope.hexValues = ['#588C7E', '#F2E394', '#F2AE72', '#D96459', '#8C4646'];
+            // $scope.hexValues = ['#588C7E', '#F2E394', '#F2AE72', '#D96459', '#8C4646'];
+            $scope.hexValues = ['#fffbd4','#ccfbd4','#99fbd4','#66fbd4','#33fbd4'];
+            // $scope.hexValues = ['#DEE7CC','#DEB8CC','#DE8ACC','#DE5CCC','#DE2ECC'];
         });
 
         $scope.changeColor = function(color){
